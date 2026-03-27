0673d987bf58a4a9e1fb6b7fdf8a7dd6b0949550
Nigel Daley <nigel.daley@cosector.com>
2026-03-27 07:44:57 +0000

    added breakdown report installation info

--- db/install.php
+++ db/install.php
@@ -25,12 +25,12 @@ function xmldb_local_mmu_rise_reportsdash_reports_install() {
     }
 
     // Coursework Settings Amendments report
-    if (!$DB->record_exists('block_reportsdash_extrep', array('classname' =>'mmu_rise_students_points_report' ))) {
+    if (!$DB->record_exists('block_reportsdash_extrep', array('classname' =>'mmu_rise_student_points_report' ))) {
 
         $externalreport = new \stdClass();
         $externalreport->namespace = "\local_mmu_rise_reportsdash_reports\\reports";
-        $externalreport->classpath = "\local_mmu_rise_reportsdash_reports\\reports\\mmu_rise_students_points_report";
-        $externalreport->classname = "mmu_rise_students_points_report";
+        $externalreport->classpath = "\local_mmu_rise_reportsdash_reports\\reports\\mmu_rise_student_points_report";
+        $externalreport->classname = "mmu_rise_student_points_report";
 
         $externalreport->dirpath = str_replace("\\", DIRECTORY_SEPARATOR, $CFG->dirroot . "\local\\mmu_rise_reportsdash_reports\\classes\\reports\\");
         $externalreport->langfile = str_replace("\\", DIRECTORY_SEPARATOR, $CFG->dirroot . "\local\\mmu_rise_reportsdash_reports\\mmu_rise_reportsdash_reports_lang.php");
@@ -39,6 +39,20 @@ function xmldb_local_mmu_rise_reportsdash_reports_install() {
 
     }
 
+    // Coursework Settings Amendments report
+    if (!$DB->record_exists('block_reportsdash_extrep', array('classname' =>'mmu_rise_student_points_breakdown_report' ))) {
+
+        $externalreport = new \stdClass();
+        $externalreport->namespace = "\local_mmu_rise_reportsdash_reports\\reports";
+        $externalreport->classpath = "\local_mmu_rise_reportsdash_reports\\reports\\mmu_rise_student_points_breakdown_report";
+        $externalreport->classname = "mmu_rise_student_points_breakdown_report";
+
+        $externalreport->dirpath = str_replace("\\", DIRECTORY_SEPARATOR, $CFG->dirroot . "\local\\mmu_rise_reportsdash_reports\\classes\\reports\\");
+        $externalreport->langfile = str_replace("\\", DIRECTORY_SEPARATOR, $CFG->dirroot . "\local\\mmu_rise_reportsdash_reports\\mmu_rise_reportsdash_reports_lang.php");
+
+        $DB->insert_record("block_reportsdash_extrep", $externalreport);
+
+    }
 
 
 }
